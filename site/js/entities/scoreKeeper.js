c57d40a9b1c1579df61dedfea13e91a15fef21fb
iris-qu <iris.xiaoyu.qu@gmail.com>
2015-05-18 15:40:45 -0700

    responsive

--- site/js/entities/scoreKeeper.js
+++ site/js/entities/scoreKeeper.js
@@ -3,7 +3,6 @@ var physicsComponent = require("../components/physics/physics");
 var collisionComponent = require("../components/collision/circle");
 
 var score = 0;
-var highScore = 0;
 
 var ScoreKeeper = function() {
 	var physics = new physicsComponent.PhysicsComponent(this);
@@ -11,7 +10,7 @@ var ScoreKeeper = function() {
     physics.position.x = -0.16;
 
     var graphics = new graphicsComponent.ScoreGraphicsComponent(this);
-    var collision = new collisionComponent.CircleCollisionComponent(this, 0.01);
+    var collision = new collisionComponent.CircleCollisionComponent(this, 0.02);
     collision.onCollision = this.onCollision.bind(this);
 
     this.components = {
@@ -20,40 +19,13 @@ var ScoreKeeper = function() {
         collision: collision
     };
 
-    var highScore = this.loadHiScore();
 };
 
 ScoreKeeper.prototype.onCollision = function() {
     var scoreIncrease = score += 1;
-    var scoreTotal = scoreIncrease/21;
+    var scoreTotal = scoreIncrease/23;
     var scoreFixed = scoreTotal.toFixed(0);
     document.getElementById("score").innerHTML = scoreFixed;
-
-    if (scoreFixed > highScore) {
-        highScore = scoreFixed;
-        document.getElementById("highScore").innerHTML = highScore;
-        this.saveHiScore();
-    }
-
-    console.log(highScore); 
-    
-};
-
-ScoreKeeper.prototype.saveHiScore = function() {
-    if (Modernizr.localstorage) {
-        window.localStorage.setItem('highScore', highScore);
-    }else {
-        console.log("Local Storage is not available with in this browser.");
-        document.getElementById("highScore").innerHTML = "unavailable";
-    }
-};
-
-ScoreKeeper.prototype.loadHiScore = function() {
-    if (Modernizr.localstorage) {
-        return parseInt(window.localStorage.getItem('highScore')) || parseInt('0');
-    }else{
-        return 0;
-    }
 };
 
 exports.ScoreKeeper = ScoreKeeper;
